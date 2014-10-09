4d9b38dfbe4b1460fcdb42fc9c3b2db057386c59
rbsrio <ricardosantos.rio.br@live.com>
2014-10-09 17:40:46 -0300

    Replace text from 2013 to 2014

--- SBWApp/targetver.h
+++ SBWApp/targetver.h
@@ -1,7 +1,7 @@
 /*
        This file is subject to the terms and conditions defined in
        file 'LICENSE.txt', which is part of this source code package.
-       Copyright (C) 2013 Ricardo B. Santos.
+       Copyright (C) 2014 Ricardo B. Santos.
        All rights reserved
 */
 #pragma once
